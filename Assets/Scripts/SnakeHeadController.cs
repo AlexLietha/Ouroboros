3ad8d51d2ad05ede498226e83206b3ab1e62494e
AlexLietha <alexlietha@gmail.com>
2025-05-21 16:05:49 -0500

    Snake Grows
    Added:
    - snake body will clone when it is 1.6 units away from the head
    - Game end statements in the snakecontroller
    
    Changed:
    - snake movement is now tied with phase rather than time

--- Assets/Scripts/SnakeHeadController.cs
+++ Assets/Scripts/SnakeHeadController.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -13,25 +13,52 @@ public class SnakeHeadController : MonoBehaviour
 
     public float startingPos;
 
+    public bool moving;
+    public float phase;
     // Start is called before the first frame update
     void Start() {
-
+        moving = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector2(route.transform.localScale.x / 2 * Mathf.Cos((speed * Time.time) + startingPos) + route.transform.position.x
-                                       , route.transform.localScale.y / 2 * Mathf.Sin((speed * Time.time) + startingPos) + route.transform.position.y);
+        if (moving)
+        {
+            Move();
+        }
+    }
+
+    private void Move()
+    {
+        phase += speed * Time.deltaTime;
+
+        // Keep phase within 0 to 2π for cleanliness (optional)
+        
+
+        transform.position = new Vector2(route.transform.localScale.x / 2 * Mathf.Cos(phase + startingPos) + route.transform.position.x
+                                       , route.transform.localScale.y / 2 * Mathf.Sin(phase + startingPos) + route.transform.position.y);
 
         Vector3 direction = transform.position - route.transform.position;
         transform.eulerAngles = new Vector3(0, 0, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg);
-        currentTrackPosition = Mathf.Abs(Mathf.Atan2(direction.y, direction.x));
+        //currentTrackPosition = Mathf.Abs(Mathf.Atan2(direction.y, direction.x));
+
+        currentTrackPosition = Mathf.Atan2(direction.y, direction.x);
+        if (currentTrackPosition < 0)
+        {
+            currentTrackPosition += 2 * Mathf.PI;
+        }
     }
 
-    public void SetTrackPosition(float position)
+
+
+    public void SetStartPosition(float position)
     {
-        currentTrackPosition = position;
+        startingPos = position;
+    }
+    public float GetStartPosition()
+    {
+       return startingPos;
     }
     public float GetTrackPosition()
     {
--- Assets/SnakeController.cs
+++ Assets/SnakeController.cs
@@ -6,7 +6,9 @@ public class SnakeController : MonoBehaviour
 {
     public GameObject SnakeHead;
     public GameObject currentSnakeBody;
+    public GameObject PrefabBody;
     public GameObject SnakeTail;
+    public GameObject route;
     public float distBetweenHeadBody;
     // Start is called before the first frame update
     void Start()
@@ -17,13 +19,45 @@ public class SnakeController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        distBetweenHeadBody = Vector3.Distance(SnakeHead.transform.position, currentSnakeBody.transform.position);
-        //Debug.Log(Vector3.Distance(SnakeHead.transform.position, currentSnakeBody.transform.position));
-        if (distBetweenHeadBody > 1.5f)
+        checkEndGame();
+        if(Vector3.Distance(SnakeHead.transform.position, currentSnakeBody.transform.position) < 0)
         {
-            Debug.Log("AHHHHHHHHHHHHHHHHHHH");
-            currentSnakeBody = Instantiate(currentSnakeBody, SnakeHead.transform.position, Quaternion.identity);
-            currentSnakeBody.GetComponent<SnakeHeadController>().SetTrackPosition(SnakeHead.GetComponent<SnakeHeadController>().GetTrackPosition() - .25f);
+            float headPos = SnakeHead.transform.position.magnitude + (2 * Mathf.PI);
+            distBetweenHeadBody = headPos - currentSnakeBody.transform.position.magnitude;
         }
+        else
+        {
+            distBetweenHeadBody = Vector3.Distance(SnakeHead.transform.position, currentSnakeBody.transform.position);
+        }
+
+        if (distBetweenHeadBody > 1.6f)
+        {
+            SpawnBody();
+        }
+
+
+
+
+
+    }
+    public void checkEndGame()
+    {
+        if ((SnakeHead.transform.position - SnakeTail.transform.position).magnitude  < .01f)
+
+        {
+            Debug.Log("End Game, Bad Ending");
+        }
+        else if(SnakeHead.GetComponent<SnakeHeadController>().health <= 0)
+        {
+            Debug.Log("End Game, Good Ending");
+        }
+    }
+
+
+    public void SpawnBody()
+    {
+       currentSnakeBody = Instantiate(PrefabBody, SnakeHead.transform.position, SnakeHead.transform.rotation);
+       currentSnakeBody.GetComponent<SnakeHeadController>().route = route;
+        currentSnakeBody.GetComponent<SnakeHeadController>().phase = SnakeHead.GetComponent<SnakeHeadController>().currentTrackPosition;
     }
 }
